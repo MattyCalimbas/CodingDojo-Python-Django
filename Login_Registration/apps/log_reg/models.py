cb289e913033f79f129a82a508bcb5b984a97b9c
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2018-12-13 13:20:33 -0800

    Add files via upload

---
+++ Login_Registration/apps/log_reg/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.EmailField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
